fc96c8422577c83a58e9f2834d4f611be4200e80
Shivam Daryanani <daryananis@gmail.com>
2013-04-22 19:22:43 -0700

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instaslide::Application.config.secret_token = '3f88c975b3973c17cf894ee40fc0531e33629f76f8090e060342147679f1fc909e8c53ef1d1964b5646cb4cb9287f7a12219119e101834d92881b86894bfa4b8'
