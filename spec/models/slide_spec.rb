7acdf0cf4049ab9ecdea3ccfb7d0f1cd7a2b9591
Shivam Daryanani <daryananis@gmail.com>
2013-04-23 09:27:09 -0700

    Test for Slide model association with photos

--- spec/models/slide_spec.rb
+++ spec/models/slide_spec.rb
@@ -6,4 +6,5 @@ describe Slide do
 	it { should validate_presence_of(:user_id) }
 	it { should validate_presence_of(:description) }
 	it { should belong_to(:user) }
+	it { should have_many(:photos) }
 end
