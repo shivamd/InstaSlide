d10926106aae35a5925f1fdfd6a61c7aca7063c5
Shivam Daryanani <daryananis@gmail.com>
2013-04-23 09:24:20 -0700

    Test for user having many photos through slides

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,5 @@ describe User do
 	it { should validate_presence_of(:name) }
 	it { should have_many(:authentications) }
 	it { should have_many(:slides) }
+	it { should have_many(:photos).through(:slides)}
 end
