e50705f744d04b93bb2b975352ef71c0733e087e
Shivam Daryanani <daryananis@gmail.com>
2013-04-23 08:46:20 -0700

    before filter authorizations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,13 @@ class ApplicationController < ActionController::Base
   	@current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def signed_in?
+  	!current_user.nil?
+  end
+
+  def redirect_unless_signed_in
+  	redirect_to root_url, notice: "Please sign in" unless signed_in?
+  end
+
   helper_method :current_user
 end
--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -1,14 +1,13 @@
 class SlidesController < ApplicationController
-
+	before_filter :redirect_unless_signed_in
 	def index 
 	end
-	
+
 	def new
 	end
 
 	def create
 	end
 
-	def destroy
-	end
+
 end
