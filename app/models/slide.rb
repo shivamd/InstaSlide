12ac28c66ffa342a14585927ea9c4d745b5a067c
Shivam Daryanani <daryananis@gmail.com>
2013-04-23 09:46:21 -0700

    Slide accepts nested attributes for photos

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,7 +1,8 @@
 class Slide < ActiveRecord::Base
-	attr_accessible :user_id, :name, :description
+	attr_accessible :user_id, :name, :description, :photos_attributes
 
 	validates :user_id, :name, :description, presence: true
 	belongs_to :user
 	has_many :photos
+	accepts_nested_attributes_for :photos
 end
