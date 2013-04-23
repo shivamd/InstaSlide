945b11dc39cf05c7e5801151f6452db74cf60c87
Shivam Daryanani <daryananis@gmail.com>
2013-04-22 19:33:43 -0700

    configuration for omniauth

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+Rails.application.config.middleware.use OmniAuth::Builder do 
+	provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
+  provider :instagram, ENV['INSTAGRAM_APP_ID'], ENV['INSTAGRAM_SECRET']
+  provider :flickr, ENV['FLICKR_APP_ID'], ENV['FLICKR_SECRET']
+end
