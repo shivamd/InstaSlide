fabedc6e3776b9ca6b9c932e4a7ee4205ef25916
Shivam Daryanani <daryananis@gmail.com>
2013-04-24 08:14:03 -0700

    Delegate authentication methods to user in order to access tokens

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
-	attr_accessible :name 
+	attr_accessible :name
 
 	validates :name, presence: true
 	has_many :authentications
 	has_many :slides
 	has_many :photos, through: :slides
+
+	delegate :facebook, :instagram, :flickr, to: :authentications
 end
