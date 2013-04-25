1e49a0272436467633605d452d91e0b95d40ee02
Shivam Daryanani <daryananis@gmail.com>
2013-04-25 07:07:29 -0700

    removed all nested attributes fields for photos

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,8 +1,7 @@
 class Slide < ActiveRecord::Base
-	attr_accessible :user_id, :name, :description, :photos_attributes
+	attr_accessible :user_id, :name, :description
 
 	validates :user_id, :name, :description, presence: true
 	belongs_to :user
 	has_many :photos
-	accepts_nested_attributes_for :photos
 end
