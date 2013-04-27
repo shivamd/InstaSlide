b944b41e44861ec10aecdb3d9d6174978fe2699c
Shivam Daryanani <daryananis@gmail.com>
2013-04-26 23:51:43 -0700

    changed methods in index view code to be more expressive.
    Changed token? method into three methods, one for each provider. Easier to stub

--- app/helpers/authentication_helper.rb
+++ app/helpers/authentication_helper.rb
@@ -3,4 +3,16 @@ module AuthenticationHelper
   def token?(authentications, provider)
   	current_user && authentications.send(provider).present?
   end
+
+  def instagram_token?
+  	current_user && current_user.instagram.present?
+  end
+
+  def facebook_token?
+  	current_user && current_user.facebook.present?
+  end
+
+  def flickr_token?
+  	current_user && current_user.flickr.present?
+  end
 end
