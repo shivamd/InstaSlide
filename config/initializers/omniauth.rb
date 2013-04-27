fb3bb99536e2e16b622c83073052c0efd854f14b
Shivam Daryanani <daryananis@gmail.com>
2013-04-26 21:28:40 -0700

    add user_photos permission to omniauth facebook in order to be able to access the users photos

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
-Rails.application.config.middleware.use OmniAuth::Builder do 
-	provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
+Rails.application.config.middleware.use OmniAuth::Builder do
+	provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], scope: "user_photos"
   provider :instagram, ENV['INSTAGRAM_APP_ID'], ENV['INSTAGRAM_SECRET']
   provider :flickr, ENV['FLICKR_APP_ID'], ENV['FLICKR_SECRET']
 end
