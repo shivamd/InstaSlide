85bd924317cf3527d47e352f68724ce273314483
Shivam Daryanani <daryananis@gmail.com>
2013-04-26 21:13:48 -0700

    extracted photo getting methods to photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,15 @@
 class PhotosController < ApplicationController
+
+	def facebook_albums
+		render json: fb_albums_info
+	end
+
+	def facebook_photos
+		render json: fb_photos(params[:album_id])
+	end
+
+	def instagram_photos
+		render json: ig_photos
+	end
+
 end
