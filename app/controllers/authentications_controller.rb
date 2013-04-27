14a4f72573454915e67835ec25af143a2dca277a
Shivam Daryanani <daryananis@gmail.com>
2013-04-26 21:01:04 -0700

    fixed omniauth error by editing controller action It kept giving an error where saying authentication failed. The problem was that the user wasn't getting saved, hence the authentication wasn't being created.

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -7,14 +7,13 @@ class AuthenticationsController < ApplicationController
 	def create
 		omniauth = request.env["omniauth.auth"]
 		auth = Authentication.from_omniauth(omniauth)
-		if auth 
+		if auth
 			session[:user_id] = auth.user_id
-		elsif current_user 
+		elsif current_user
 		  current_user.authentications.create!(provider: omniauth['provider'], uid: omniauth['uid'], access_token: omniauth.credentials.token)
 		else
-			user = User.new(name: omniauth.info.name)
-		  user.authentications.build(provider: omniauth['provider'], uid: omniauth['uid'], access_token: omniauth.credentials.token)
-		  user.save!
+			user = User.create(name: omniauth.info.name)
+		  user.authentications.create(provider: omniauth['provider'], uid: omniauth['uid'], access_token: omniauth.credentials.token)
 		end
 		redirect_to root_url
 	end
